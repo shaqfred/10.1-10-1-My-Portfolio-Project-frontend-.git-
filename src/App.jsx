677bbe187d5d724f9bee036c6c4c2831348e9835
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-11-19 10:23:50 -0500

    first commit-installing dependencies

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import IndexPage from
+import CreatePage from 
+import HomePage from
+import ShowPage from
+import NavBar from
+import EditPage from
+import ErrorPage from
+import './App.css'
+
+function App() {
+  
+
+  return (
+   
+        
+  
+  )
+}
+
+export default App
